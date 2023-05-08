f7379a1009b4e4035edae2161ceb1d333299d272
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-08 12:02:23 -0400

    initial commit

--- index.js
+++ index.js
@@ -1,5 +1,21 @@
 "use strict";
+// Basic Types
 let age = 25;
 let hobby = "Soccer";
 let isAdult = true;
 let fruit = "apple";
+// Arrays
+let fruits = ["Apple", "Orange"];
+let arr = [22, "a", true];
+let numArr = [[1], [2]];
+// Tuples
+let employee = [1, "Jack"];
+let employees = [
+    [2, "Sarah"],
+    [3, "Jack"]
+];
+// Objects
+let car = {
+    brand: "Lexus",
+    year: 2020
+};
--- index.ts
+++ index.ts
@@ -1,4 +1,27 @@
+// Basic Types
 let age:number = 25;
 let hobby : string = "Soccer";
 let isAdult :boolean=true;
-let fruit: string = "apple"
+let fruit: any = "apple";
+
+// Arrays
+let fruits:string[] = ["Apple", "Orange"]
+let arr:any[] = [22,"a",true]
+let numArr:number[][] = [[1],[2]]
+
+// Tuples
+
+let employee:[number, string] = [1, "Jack"]
+let employees: [number, string][]= [
+    [2, "Sarah"],
+    [3, "Jack"]
+]
+
+// Objects
+let car: {
+    brand: string,
+    year:number
+} = {
+    brand: "Lexus",
+    year:2020
+}
