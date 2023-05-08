aadafc344244d84a69a66acc8270f014e4c240e6
Seth Dimmer <sethdimmer1@gmail.com>
2023-05-08 11:28:23 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,5 @@
+"use strict";
+let age = 25;
+let hobby = "Soccer";
+let isAdult = true;
+let fruit = "apple";
---
+++ index.ts
@@ -0,0 +1,4 @@
+let age:number = 25;
+let hobby : string = "Soccer";
+let isAdult :boolean=true;
+let fruit: string = "apple"
